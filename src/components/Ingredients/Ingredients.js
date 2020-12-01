94a0ff87822b1a8d529d7ccbe67ccf1c2018f863
Janko <mailodjanka@gmail.com>
2020-12-01 13:06:23 +0100

    Adds addIngredientHandler and passes as a props to submit ingredient form

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -6,13 +6,21 @@ import IngredientList from "./IngredientList";
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((previousIngredient) => [
+      ...previousIngredient,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients}/>
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
