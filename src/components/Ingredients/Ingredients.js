35d40322680c9008584a5183b17b5b65feae0c6f
Janko <mailodjanka@gmail.com>
2020-12-01 15:05:46 +0100

    Fixes issue with loading ings from a server

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -5,24 +5,23 @@ import Search from "./Search";
 import IngredientList from "./IngredientList";
 
 function Ingredients() {
-  useEffect(() => {
-    fetch("https://hooks-d131d.firebaseio.com/ingredients.json/").then(res => {
-      res.json()
-    }).then(responseData => {
-      const loadedIngredients = []
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        })
-        
-      }
-      setUserIngredients(loadedIngredients)
-    })
-  }, [])
-
   const [userIngredients, setUserIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch("https://hooks-d131d.firebaseio.com/ingredients.json/")
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
 
   const addIngredientHandler = (ingredient) => {
     fetch("https://hooks-d131d.firebaseio.com/ingredients.json/", {
